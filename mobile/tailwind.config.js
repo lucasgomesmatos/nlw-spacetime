d80c1330b27abadbb0d236b27d7b71ce2e3e2e1c
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-17 22:49:57 -0300

    react e react-native

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: ['./App.tsx', './app/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
