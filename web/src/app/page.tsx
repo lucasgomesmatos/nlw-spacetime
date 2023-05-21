3e42c447470b069bd402da19dda7f29ced7d6e3b
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-21 16:16:10 -0300

    logout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,30 +1,5 @@
-import { Blur } from "@/components/blur";
-import { Copyright } from "@/components/copyright";
 import { EmptyMemories } from "@/components/empty-memories";
-import { Hero } from "@/components/hero";
-import { Profile } from "@/components/profile";
-import { SignIn } from "@/components/signIn";
-import { Stripes } from "@/components/stripes";
-import { cookies } from "next/headers";
 
 export default function Home() {
-  const isAuthenticated = cookies().has("token");
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        <Blur />
-        <Stripes />
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      {/* Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  );
+  return <EmptyMemories />;
 }
