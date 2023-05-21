b22536e55e73cb98559237782f144b37e2c62b69
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-21 14:20:58 -0300

    auth next

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,17 +2,21 @@ import { Blur } from "@/components/blur";
 import { Copyright } from "@/components/copyright";
 import { EmptyMemories } from "@/components/empty-memories";
 import { Hero } from "@/components/hero";
+import { Profile } from "@/components/profile";
 import { SignIn } from "@/components/signIn";
 import { Stripes } from "@/components/stripes";
+import { cookies } from "next/headers";
 
 export default function Home() {
+  const isAuthenticated = cookies().has("token");
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <Blur />
         <Stripes />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
