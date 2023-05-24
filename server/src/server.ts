ed11b582afd8a68d29a2b66aab8301c22e06222e
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-23 21:45:59 -0300

    upload file

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,6 +29,6 @@ app.register(uploadRoutes)
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
   })
   .then(() => console.log('🚀 HTTP server running on http://localhost:3333'))
